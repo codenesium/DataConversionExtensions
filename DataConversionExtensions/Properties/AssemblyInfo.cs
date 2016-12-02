387411027246223c8692eba7520bb6e286b62a03
Travis Taylor <codenesium@gmail.com>
2016-12-02 15:31:37 -0600

    Updated assembly info

--- DataConversionExtensions/Properties/AssemblyInfo.cs
+++ DataConversionExtensions/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DataConversionExtensions")]
+[assembly: AssemblyTitle("Codenesium.DataConversionExtensions")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DataConversionExtensions")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("Codenesium.DataConversionExtensions")]
+[assembly: AssemblyCopyright("Copyright Codenesium LLC 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
