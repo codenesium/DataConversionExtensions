326de01b465b16e403b0c96257e1a44e96511af7
Travis Taylor <codenesium@gmail.com>
2017-01-04 18:27:38 -0600

    Update json library

--- DataConversionExtensions/StringExtensions.cs
+++ DataConversionExtensions/StringExtensions.cs
@@ -1,9 +1,23 @@
 ﻿using System;
 using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Codenesium.DataConversionExtensions
 {
     public static class StringExtensions
     {
+        public static bool IsValidJson(this string obj)
+        {
+            try
+            {
+                JObject.Parse(obj);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
     }
 }
