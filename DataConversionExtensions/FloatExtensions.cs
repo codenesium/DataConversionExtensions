927819d316b6030abf1ad1f889ce1147cd4352a3
Travis Taylor <codenesium@gmail.com>
2019-02-05 16:12:37 -0600

    added ToFloat conversion

---
+++ DataConversionExtensions/FloatExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Codenesium.DataConversionExtensions
+{
+    public static class FloatExtensions
+    {
+        public static float ToFloat(this float obj)
+        {
+            return obj;
+        }
+
+        public static Nullable<float> ToNullableFloat(this Nullable<float> obj)
+        {
+            if (obj == null)
+            {
+                return null;
+            }
+            else
+            {
+				return obj;
+            }
+        }
+
+        public static float ToFloat(this string obj)
+        {
+            return ToNullableFloat(obj) ?? 0;
+        }
+
+        public static Nullable<float> ToNullableFloat(this string obj)
+        {
+            if (obj == null)
+            {
+                return null;
+            }
+            else
+            {
+				float result = 0;
+                if (float.TryParse(obj, out result))
+                {
+                    return result;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public static float ToFloat(this object obj)
+        {
+            return ToFloat(obj.ToString());
+        }
+    }
+}
---
+++ Tests/ToFloatTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using NUnit.Framework;
+using Codenesium.DataConversionExtensions;
+namespace DataConversionExtensionsTests
+{
+    [TestFixture]
+    public class ToFloatTests
+    {
+        [Test]
+        public void ToFloat_NullString_0()
+        {
+            string testString = null;
+            Assert.AreEqual(0, testString.ToFloat());
+        }
+
+        [Test]
+        public void ToFloat_34534123_34534123()
+        {
+            string testString = "34534.123";
+            Assert.AreEqual((float)34534.123, testString.ToFloat());
+        }
+
+        [Test]
+        public void ToFloat_ABC123_0()
+        {
+            string testString = "ABC123";
+            Assert.AreEqual(0, testString.ToFloat());
+        }
+
+        [Test]
+        public void ToNullableFloat_NullString_null()
+        {
+            string testString = null;
+            Assert.AreEqual(null, testString.ToNullableFloat());
+        }
+
+        [Test]
+        public void ToNullableFloat_ABC123_null()
+        {
+            string testString = "ABC123";
+            Assert.AreEqual(null, testString.ToNullableFloat());
+        }
+
+        [Test]
+        public void ToNullableFloat_530945690412_530945690412()
+        {
+            string testString = "5309456904.12";
+
+            Assert.AreEqual((float)5309456904.12, testString.ToNullableFloat());
+        }
+
+    }
+}
